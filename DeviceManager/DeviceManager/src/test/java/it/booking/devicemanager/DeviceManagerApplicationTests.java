c3aa081eb7b7f9864983c4f069a7e650a8311619
eliaspinna <eliaspinna99@gmail.com>
2024-08-18 18:14:16 +0200

    creazione packages

---
+++ DeviceManager/DeviceManager/src/test/java/it/booking/devicemanager/DeviceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.booking.devicemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
